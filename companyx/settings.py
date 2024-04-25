eab18f889bf4ee3ba40d3445c93ea439cf720bbc
Thabo Mbatha <thabothomasmbatha@gmail.com>
2024-04-25 15:47:55 +0200

    Work PC

--- companyx/settings.py
+++ companyx/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates', 'accounts')
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 LOGIN_REDIRECT_URL = 'landing_page'
